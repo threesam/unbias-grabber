347032ce73e466ea5ae1db3ee39c05f0377c169f
threesam <salvatoredangelo@protonmail.com>
2021-05-21 08:54:50 -0400

    rwent headless, added all code to one file - in this case, it was actually simpler to not overoptimize

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 require('dotenv').config()
-
 const NewsAPI = require('newsapi')
 const fs = require('fs')
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY)
 const { format } = require('date-fns')
+const stringTokenizer = require('string-punctuation-tokenizer')
+const prepositions = require('title-case-minors')
+const uselessWords = prepositions
+  .concat([
+    'has', 'new', 'says', 'one', 'over', 'are', 'not', 'first', 'about',
+    'this', 'into', 'was', 'after', 'what', 'will', 'have', 'like', 'back',
+    'day', 'year', 'month', 'reuters', 'say', 'two', 'just', 'you', 'its', 'get',
+    'still', 'here', 'may',
+  ])
+
+const faunadb = require('faunadb'),
+  q = faunadb.query
+
+const client = new faunadb.Client({ secret: process.env.FAUNA_KEY })
+
+
+function getWordsfromTitles(titles) {
+  return titles
+    .map(title => stringTokenizer.tokenize({ text: `${title}` })) // turn title into array of words
+    .flat()
+    .map(word => word.toLowerCase())
+    .filter(item => Number.isNaN(+item) && item.length > 2) // filter out numbers and words shorter than two letters
+    .filter(item => !uselessWords.includes(item)) // filter prepositions and other short undescriptive words
+}
+
+function countAndSortWords(words) {
+  const count = words
+    .reduce((map, word) => {
+      map[word] = (map[word] || 0) + 1
+      return map
+    }, Object.create(null))
+
+
+  const sortedCount = Object.entries(count)
+    .sort((a, b) => b[1] - a[1])
+
+  return sortedCount
+}
 
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index
@@ -13,10 +50,12 @@ function onlyUnique(value, index, self) {
 // All options are optional
 async function getData() {
   try {
+    const category = "general" // politics
+
     const fetch = await newsapi.v2.sources({
       language: 'en',
       country: 'us',
-      category: 'general'
+      category
     })
 
     const sources = await fetch.sources
@@ -31,22 +70,39 @@ async function getData() {
         return article.title
       }).filter(onlyUnique))
 
+      const words = getWordsfromTitles(titles)
+
+      const sortedWords = countAndSortWords(words)
+
       return {
         source,
-        titles
+        words: sortedWords
       }
     }))
-
-    data.date = Date.now()
-    console.log(data)
+    
+    const day = {
+      date: Date.now(),
+      category,
+      sources: data
+    }
 
     // timestamp and log results
-    fs.writeFileSync(`./web/public/data/${format(Date.now(), 'yyyy-MM-dd')}.json`, JSON.stringify(data))
+    fs.writeFileSync(`./data/${format(Date.now(), 'yyyy-MM-dd')}.json`, JSON.stringify(day))
 
-    return data
+    client.query(
+      q.Create(
+        q.Collection('daily_data'),
+        {
+          data: { ...day }
+        }
+      )
+    ).catch(e => console.error(e))
+
+    return day
   } catch (e) {
     console.error(e)
   }
 }
 
 getData()
+
