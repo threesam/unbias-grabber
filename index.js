ce3b7f6fba6efb864d34ea839e489a5995831b5b
threesam <salvatoredangelo@protonmail.com>
2021-05-22 22:53:13 -0400

    removed sort of associative array

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function countAndSortWords(words) {
   const sortedCount = Object.entries(count)
     .sort((a, b) => b[1] - a[1])
 
-  return sortedCount
+  return count
 }
 
 function onlyUnique(value, index, self) {
